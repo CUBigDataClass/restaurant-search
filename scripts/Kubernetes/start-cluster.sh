b9312c9a0386a53b5f91413fc146bfa1af4bf7bb
KDBhat95 <krithika.9509@gmail.com>
2020-04-15 21:17:35 -0600

    Adding intital scripts for Elasticsearch setup

--- scripts/Kubernetes/start-cluster.sh
+++ scripts/Kubernetes/start-cluster.sh
@@ -1,4 +1,5 @@
+export PROJECT=$(gcloud info --format='value(config.project)')			    # Set PROJECT=project_id
 gcloud config set compute/zone us-west1-b
-gcloud container clusters create --preemptible bigdata-cluster --tags=allow-5000
-gcloud container clusters get-credentials bigdata-cluster
-kubectl get nodes
+gcloud container clusters create --preemptible bigdata-cluster --tags=allow-5000    # Create the cluster; by default 3 nodes are present in the cluster
+gcloud container clusters get-credentials bigdata-cluster                           # Fetch credentials for a running cluster; credentials are written to HOME/.kube/config
+kubectl get nodes                                                                   # Gets details on the nodes of the cluster created
