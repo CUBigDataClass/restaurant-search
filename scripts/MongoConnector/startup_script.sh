a4a78b3bca0e45cd314932984046f705a5d80d95
KDBhat95 <krithika.9509@gmail.com>
2020-04-23 22:56:51 -0600

    Updated startup_script of mongoconnector

--- scripts/MongoConnector/startup_script.sh
+++ scripts/MongoConnector/startup_script.sh
@@ -1,4 +1,6 @@
 apt-get update
+apt-get install -y curl
 apt-get install -y python3 python3-pip
+curl --header 'Content-Type:application/json' --request PUT http://elasticsearch-0.es:9200/mydb_restaurants -d '{"mappings": {"properties": {"location": {"type": "geo_point"}}}}'
 pip3 install 'mongo-connector[elastic5]' 'elastic2-doc-manager[elastic5]'
 mongo-connector -m "mongodb://mongo-0.mongo,mongo-1.mongo,mongo-2.mongo" -c mongoconnector.json	# Connects mongodb to elasticsearch
