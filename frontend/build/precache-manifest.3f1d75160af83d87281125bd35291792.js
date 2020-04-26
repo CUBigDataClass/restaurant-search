6b07f420237cf131f2a676c2235d473810a4097c
rajchandak <raj.chandak@colorado.edu>
2020-04-26 00:33:57 -0600

    Added yaml and build files for deployment on GAE

---
+++ frontend/build/precache-manifest.3f1d75160af83d87281125bd35291792.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1924984f91aa5c9d6ff397b44a2a470c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e220c1cb27c588225272",
+    "url": "/static/css/2.58c6c8cd.chunk.css"
+  },
+  {
+    "revision": "401a6b538a9e86bcf6da",
+    "url": "/static/css/main.ba8c3b51.chunk.css"
+  },
+  {
+    "revision": "e220c1cb27c588225272",
+    "url": "/static/js/2.4a389110.chunk.js"
+  },
+  {
+    "revision": "9b1738102eb5ceb2256f77ddd6e6be32",
+    "url": "/static/js/2.4a389110.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "401a6b538a9e86bcf6da",
+    "url": "/static/js/main.0f91d891.chunk.js"
+  },
+  {
+    "revision": "8d3c04da84f9e31d1051",
+    "url": "/static/js/runtime-main.9e76eb68.js"
+  },
+  {
+    "revision": "ca938cc69789bf87811b261025462bd8",
+    "url": "/static/media/beer.ca938cc6.svg"
+  },
+  {
+    "revision": "166e9885da730c611498e9613cb9c01c",
+    "url": "/static/media/burger.166e9885.svg"
+  },
+  {
+    "revision": "f673583b2fff81a3c029a09c0356baea",
+    "url": "/static/media/pizza.f673583b.svg"
+  },
+  {
+    "revision": "69985c09ad6128c49c796135e22e87cc",
+    "url": "/static/media/review.69985c09.svg"
+  },
+  {
+    "revision": "ae90d59f94a0352ca1b55269827e36f1",
+    "url": "/static/media/search.ae90d59f.svg"
+  }
+]);
